66f326d1e2da0224e8a5fba50cb7c2f31cef140a
masum065 <masumbillah065@gmail.com>
2020-10-03 15:20:32 +0600

    addded date

--- routes/index.js
+++ routes/index.js
@@ -3,40 +3,61 @@ var router = express.Router();
 const multer = require('multer');
 
 const fs = require('fs');
-const {
-  promisify
-} = require('util');
+const { promisify } = require('util');
 const pipeline = promisify(require('stream').pipeline);
-
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'Express'
+    title: 'Express',
+  });
+});
+const upload = multer();
+const MongoClient = require('mongodb').MongoClient;
+const uri =
+  'mongodb+srv://rider:XBy6BoY1A6WyHZ8e@cluster1.fwfle.mongodb.net/events?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const eventCollections = client.db('events').collection('programs');
+
+  router.post('/upload', upload.single('file'), async function (
+    req,
+    res,
+    next
+  ) {
+    const {
+      file,
+      body: { name, date },
+    } = req;
+    const fileName = name + file.detectedFileExtension;
+    await pipeline(
+      file.stream,
+      fs.createWriteStream(`${__dirname}/../public/images/${fileName}`)
+    );
+    res.send(req.file);
+    console.log(fileName);
+    console.log({
+      url: `${__dirname}/images/${fileName}`,
+      name: name,
+    });
+
+    const eventData = {
+      url: `${__dirname}/images/${fileName}`,
+      name: name,
+      date: date,
+    };
+
+    eventCollections.insertOne(eventData).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
   });
 });
 
-const upload = multer();
-router.post("/upload", upload.single("file"), async function (req, res, next) {
-  const {
-    file,
-    body: {
-      name
-    }
-  } = req;
-  const fileName = name + file.detectedFileExtension;
-  await pipeline(
-    file.stream,
-    fs.createWriteStream(`${__dirname}/../public/images/${fileName}`)
-  );
-  res.send(req.file);
-  console.log(fileName);
-  console.log(`/images/${fileName}`);
-});
-
-
 router.get('/images', (req, res) => {
-  res.send(`${__dirname}/../public/images`)
-})
+  res.send(`${__dirname}/../public/images`);
+});
 
 module.exports = router;
